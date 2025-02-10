88365915b8fceece7798077694e33b9ac549a8fb
a.kerezbekov <arsenkerezbekov7@gmail.com>
2025-02-10 17:13:08 +0600

    finish all project

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.burger')
 const modal = document.querySelector('.modal')
 const close = document.querySelector('.close')
+const buttons = document.querySelectorAll('.modal a')
 
 burger.addEventListener('click', () => {
     modal.style.display = 'flex'
@@ -9,3 +10,8 @@ close.addEventListener('click', () => {
     modal.style.display = 'none'
 })
 
+buttons.forEach(el => {
+    el.addEventListener('click', () => {
+        modal.style.display = 'none'
+    })
+})
